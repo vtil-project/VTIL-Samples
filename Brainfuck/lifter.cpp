b3da445e206e38b0261be6701c43f8a9dd496df6
Layle <luca@marcelli.ch>
2020-06-24 16:13:31 +0200

    oop style

---
+++ Brainfuck/lifter.cpp
@@ -0,0 +1,134 @@
+#include "lifter.hpp"
+
+using namespace bf;
+
+lifter::lifter(std::string program) : m_program(std::move(program)), m_block(nullptr), m_vip(0) {};
+
+void lifter::lift()
+{
+    m_block = vtil::basic_block::begin(0x0);
+    m_branches = std::list<vtil::vip_t>();
+
+    for(auto instruction : m_program)
+    {
+        handle_instruction(instruction);
+    }
+
+    m_block->vexit(0ull);
+
+    vtil::logger::log("Lifted! Running optimizations...\n\n");
+}
+
+void lifter::optimize()
+{
+    vtil::optimizer::apply_each<
+            vtil::optimizer::profile_pass,
+            vtil::optimizer::collective_pass
+    >{}(m_block->owner);
+
+    vtil::logger::log("\nOptimizations applied! Here's the VTIL:\n\n");
+}
+
+void lifter::dump()
+{
+    vtil::debug::dump(m_block->owner);
+}
+
+void lifter::save(std::string path)
+{
+    vtil::logger::log("\nSaving VTIL to %s", path);
+    vtil::save_routine(m_block->owner, path);
+}
+
+void lifter::handle_instruction(char instruction)
+{
+    switch (instruction)
+    {
+        case '>':
+            m_block->add(vtil::REG_SP, 1);
+            break;
+        case '<':
+            m_block->sub(vtil::REG_SP, 1);
+            break;
+        case '+':
+            handle_inc();
+            break;
+        case '-':
+            handle_dec();
+            break;
+        case '[':
+            handle_te();
+            break;
+        case ']':
+            handle_tne();
+            break;
+        case '.':
+            handle_print();
+            break;
+        case ',':
+            handle_read();
+            break;
+        default:
+            break;
+    }
+}
+
+void lifter::handle_inc()
+{
+    auto current_value = m_block->tmp(8);
+    m_block->ldd(current_value, vtil::REG_SP, vtil::make_imm(0ull));
+    m_block->add(current_value, 1);
+    m_block->str(vtil::REG_SP, vtil::make_imm(0ull), current_value);
+}
+
+void lifter::handle_dec()
+{
+    auto current_value = m_block->tmp(8);
+    m_block->ldd(current_value, vtil::REG_SP, vtil::make_imm(0ull));
+    m_block->sub(current_value, 1);
+    m_block->str(vtil::REG_SP, vtil::make_imm(0ull), current_value);
+}
+
+void lifter::handle_te()
+{
+    auto [tmp, cond] = m_block->tmp(8, 1);
+    m_block->ldd(tmp, vtil::REG_SP, 0);
+    m_block->te(cond, tmp, 0);
+    m_block->js(cond, ++m_vip, vtil::invalid_vip);
+
+    m_branches.push_back(m_block->entry_vip);
+    m_block = m_block->fork(m_vip);
+}
+
+void lifter::handle_tne()
+{
+    auto [tmp, cond] = m_block->tmp(8, 1);
+    m_block->ldd(tmp, vtil::REG_SP, 0);
+    m_block->tne(cond, tmp, 0);
+    m_block->js(cond, m_block->entry_vip, ++m_vip);
+
+    update_branch();
+    m_block = m_block->fork(m_vip);
+}
+
+void lifter::handle_print()
+{
+    m_block->ldd(x86_reg::X86_REG_AL, vtil::REG_SP, 0);
+    m_block->vpinr(x86_reg::X86_REG_AL); // make sure this doesn't get optimized away
+    m_block->vemit('.');
+}
+
+void lifter::handle_read()
+{
+    m_block->vemit(',');
+    m_block->vpinw(x86_reg::X86_REG_AL); // make sure this doesn't get optimized away
+    m_block->str(vtil::REG_SP, 0, x86_reg::X86_REG_AL);
+}
+
+void lifter::update_branch()
+{
+    auto matching_vip = m_branches.back(); m_branches.pop_back();
+    auto matching_block = m_block->owner->explored_blocks[matching_vip];
+    matching_block->stream.back().operands[2].imm().u64 = m_vip;
+    m_block->fork(m_block->entry_vip); // link the previously undefined block
+}
--- Brainfuck/main.cpp
+++ Brainfuck/main.cpp
@@ -1,109 +1,8 @@
 #include <string>
 #include <vtil/vtil>
+#include "lifter.hpp"
 
 #pragma comment(linker, "/STACK:67108864")
-
-void handle_inc(vtil::basic_block*& block);
-void handle_dec(vtil::basic_block*& block);
-void handle_print(vtil::basic_block*& block);
-void handle_read(vtil::basic_block*& block);
-void handle_te(vtil::basic_block*& block, vtil::vip_t& vip, std::list<vtil::vip_t>& blocks);
-void handle_tne(vtil::basic_block*& block, vtil::vip_t& vip, std::list<vtil::vip_t>& blocks);
-void handle_instruction(vtil::basic_block*& block, char instruction, vtil::vip_t& vip, std::list<vtil::vip_t>& blocks);
-void update_branch(vtil::basic_block*& block, vtil::vip_t& vip, std::list<vtil::vip_t>& blocks);
-
-void handle_inc(vtil::basic_block*& block)
-{
-    auto current_value = block->tmp(8);
-    block->ldd(current_value, vtil::REG_SP, vtil::make_imm(0ull));
-    block->add(current_value, 1);
-    block->str(vtil::REG_SP, vtil::make_imm(0ull), current_value);
-}
-
-void handle_dec(vtil::basic_block*& block)
-{
-    auto current_value = block->tmp(8);
-    block->ldd(current_value, vtil::REG_SP, vtil::make_imm(0ull));
-    block->sub(current_value, 1);
-    block->str(vtil::REG_SP, vtil::make_imm(0ull), current_value);
-}
-
-void handle_te(vtil::basic_block*& block, vtil::vip_t& vip, std::list<vtil::vip_t>& blocks)
-{
-    auto [tmp, cond] = block->tmp(8, 1);
-    block->ldd(tmp, vtil::REG_SP, 0);
-    block->te(cond, tmp, 0);
-    block->js(cond, ++vip, vtil::invalid_vip);
-
-    blocks.push_back(block->entry_vip);
-    block = block->fork(vip);
-}
-
-void handle_tne(vtil::basic_block*& block, vtil::vip_t& vip, std::list<vtil::vip_t>& blocks)
-{
-    auto [tmp, cond] = block->tmp(8, 1);
-    block->ldd(tmp, vtil::REG_SP, 0);
-    block->tne(cond, tmp, 0);
-    block->js(cond, block->entry_vip, ++vip);
-
-    update_branch(block, vip, blocks);
-    block = block->fork(vip);
-}
-
-void handle_print(vtil::basic_block*& block)
-{
-    block->ldd(x86_reg::X86_REG_AL, vtil::REG_SP, 0);
-    block->vpinr(x86_reg::X86_REG_AL); // make sure this doesn't get optimized away
-    block->vemit('.');
-}
-
-void handle_read(vtil::basic_block*& block)
-{
-    block->vemit(',');
-    block->vpinw(x86_reg::X86_REG_AL); // make sure this doesn't get optimized away
-    block->str(vtil::REG_SP, 0, x86_reg::X86_REG_AL);
-}
-
-void update_branch(vtil::basic_block*& block, vtil::vip_t& vip, std::list<vtil::vip_t>& blocks)
-{
-    auto matching_vip = blocks.back(); blocks.pop_back();
-    auto matching_block = block->owner->explored_blocks[matching_vip];
-    matching_block->stream.back().operands[2].imm().u64 = vip;
-    block->fork(block->entry_vip); // link the previously undefined block
-}
-
-void handle_instruction(vtil::basic_block*& block, char instruction, vtil::vip_t& vip, std::list<vtil::vip_t>& blocks)
-{
-    switch (instruction)
-    {
-        case '>':
-            block->add(vtil::REG_SP, 1);
-            break;
-        case '<':
-            block->sub(vtil::REG_SP, 1);
-            break;
-        case '+':
-            handle_inc(block);
-            break;
-        case '-':
-            handle_dec(block);
-            break;
-        case '[':
-            handle_te(block, vip, blocks);
-            break;
-        case ']':
-            handle_tne(block, vip, blocks);
-            break;
-        case '.':
-            handle_print(block);
-            break;
-        case ',':
-            handle_read(block);
-            break;
-        default:
-            break;
-    }
-}
 
 std::pair<std::string, std::optional<std::string>> handle_arguments(int argc, char* argv[])
 {
@@ -132,31 +31,10 @@ int main(int argc, char* argv[])
 {
     auto [program, output] = handle_arguments(argc, argv);
 
-    auto block = vtil::basic_block::begin(0x0);
-    auto blocks = std::list<vtil::vip_t>();
+    bf::lifter lifter(program);
+    lifter.lift();
+    lifter.optimize();
+    lifter.dump();
 
-    vtil::vip_t vip = 0;
-    for(auto instruction : program)
-    {
-        handle_instruction(block, instruction, vip, blocks);
-    }
-
-    block->vexit(0ull);
-
-    vtil::logger::log("Lifted! Running optimizations...\n\n");
-
-    vtil::optimizer::apply_each<
-        vtil::optimizer::profile_pass,
-        vtil::optimizer::collective_pass
-    >{}(block->owner);
-
-    vtil::logger::log("\nOptimizations applied! Here's the VTIL:\n\n");
-
-    vtil::debug::dump(block->owner);
-
-    if(output)
-    {
-        vtil::logger::log("\nSaving VTIL to %s", output.value());
-        vtil::save_routine(block->owner, output.value());
-    }
+    if(output) lifter.save(output.value());
 }
